6f037a2812243a8c9f1ac7e030b5e22952f2d255
ArtemMahazIPZ <159789232+ArtemMahazIPZ@users.noreply.github.com>
2024-05-13 15:36:57 +0300

    Add files via upload

---
+++ Lab02/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Authenticator authenticator1 = Authenticator.GetInstance();
+        Authenticator authenticator2 = Authenticator.GetInstance();
+
+    
+        if (authenticator1 == authenticator2)
+        {
+            Console.WriteLine("Це один і той же екземпляр Authenticator!");
+        }
+        else
+        {
+            Console.WriteLine("Це різні екземпляри Authenticator!");
+        }
+    }
+}
