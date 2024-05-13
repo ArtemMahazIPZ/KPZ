9f4ca4d6a96c9162bde33f70b016e2a915fa9660
ArtemMahazIPZ <159789232+ArtemMahazIPZ@users.noreply.github.com>
2024-05-13 15:36:06 +0300

    Add files via upload

---
+++ Task3/Authenticator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Authenticator
+{
+    private static Authenticator _instance;
+    private static readonly object _lock = new object();
+
+    private Authenticator()
+    {
+        
+    }
+
+    public static Authenticator GetInstance()
+    {
+        if (_instance == null)
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Authenticator();
+                }
+            }
+        }
+
+        return _instance;
+    }
+}
