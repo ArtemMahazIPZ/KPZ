c0d197aa0ab5bb0b664c37a85c3c8a52e4f4c307
ArtemMahazIPZ <159789232+ArtemMahazIPZ@users.noreply.github.com>
2024-05-13 15:28:09 +0300

    Add files via upload

---
+++ Lab02/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        var factory = new DeviceFactory();
+
+        var smartphone1 = factory.CreateDevice("Смартфон", "IProne");
+        var smartphone2 = factory.CreateDevice("Смартфон", "Balaxy");
+        var ebook = factory.CreateDevice("Електронна книжка", "Kiaomi");
+        var laptop = factory.CreateDevice("Ноутбук", "Kiaomi");
+        var netbook = factory.CreateDevice("Нетбук", "Balaxy");
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        smartphone1.DisplayInfo();
+        smartphone2.DisplayInfo();
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        ebook.DisplayInfo();
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        laptop.DisplayInfo();
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Red;
+        netbook.DisplayInfo();
+        Console.ResetColor();
+
+        Console.ReadLine();
+    }
+}
